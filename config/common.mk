49da5efa6c7c06fba2aa20d01101ad949b0bdc90
YOUNG HO CHA <ganadist@gmail.com>
2018-06-26 00:46:49 -0500

    Lock rotation on SetupWizard
    This avoids any jank that may come from setup wizards that are not
    optimized for landscape views.
    
    Change-Id: I5f5506ced71deda3ba0e44a222c7d6b21907460f

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,7 @@ PRODUCT_GENERIC_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# General additions
 PRODUCT_GENERIC_PROPERTIES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
@@ -21,6 +22,7 @@ PRODUCT_GENERIC_PROPERTIES += \
     persist.sys.disable_rescue=true \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0 \
+    ro.setupwizard.rotation_locked=true \
     ro.build.selinux=1
 
 PRODUCT_COPY_FILES += \
